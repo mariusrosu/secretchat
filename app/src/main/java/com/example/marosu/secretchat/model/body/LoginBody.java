c07c0cce4d12b6e4b2556008ff75845bcd8eac73
ENDAVA\marosu <Marius.Rosu@endava.com>
2017-08-29 10:23:14 +0300

    Serialize login names

--- app/src/main/java/com/example/marosu/secretchat/model/body/LoginBody.java
+++ app/src/main/java/com/example/marosu/secretchat/model/body/LoginBody.java
@@ -1,10 +1,15 @@
 package com.example.marosu.secretchat.model.body;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Marius-Andrei Rosu on 8/28/2017.
  */
 public class LoginBody {
+    @SerializedName("email")
     private String email;
+
+    @SerializedName("password")
     private String password;
 
     public LoginBody(String email, String password) {
